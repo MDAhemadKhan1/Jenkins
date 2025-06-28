4b845b8874bd3882c9e560510f5a27b1c92d4f87
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-06-28 23:27:18 +0530

    adding

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "this" {
   vpc_security_group_ids  = [aws_security_group.allow_tls.id]
   instance_type           = "t3.micro"
 tags = {
-    Name = "terraform-demo"
+    Name = "jenkins"
   }
 
 }
