14d036bd994378a131db097b5c9bda907b7afc18
MDAhemadKhan1 <ahemadahemad15526@gmail.com>
2025-06-29 00:13:53 +0530

    adding

--- ec2.tf
+++ ec2.tf
@@ -24,6 +24,13 @@ ingress {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 
   egress {
     from_port        = 0
